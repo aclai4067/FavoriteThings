54b23c1e01ab71c5b5a98f03d8a8a8851921baef
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-11 21:41:38 -0500

    WIP

---
+++ FavoriteThings/Media/MediaBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.Media
+{
+    abstract class MediaBase
+    {
+        public Genre Genre { get; set; }
+        public string Title { get; set; }
+        public decimal Rating { get; set; }
+
+        public void Rate(decimal rating)
+        {
+            Rating = rating;
+            Console.WriteLine($"You have updated the rating of {Title} to {Rating} stars!");
+        }
+    }
+}
--- FavoriteThings/Media/Movies.cs
+++ FavoriteThings/Media/Movies.cs
@@ -4,30 +4,21 @@ using System.Text;
 
 namespace FavoriteThings.Media
 {
-    class Movies
-    {
-        public Genre MovieGenre { get; }
-        public string Title { get; }
-        public decimal Rating { get; set; }
+    class Movies : MediaBase
+    {        
         private int _timesWatched;
 
         public Movies(string title, Genre genre)
         {
             Title = title;
-            MovieGenre = genre;
+            Genre = genre;
             _timesWatched = 1;
         }
 
         public void Watch()
         {
             _timesWatched += 1;
-            Console.WriteLine($"You have watched the {MovieGenre} movie, {Title}, {_timesWatched} times.");
-        }
-
-        public void Rate(decimal rating)
-        {
-            Rating = rating;
-            Console.WriteLine($"You have updated the rating of {Title} to {Rating} stars!");
+            Console.WriteLine($"You have watched the {Genre} movie, {Title}, {_timesWatched} times.");
         }
     }
 }
